089f2486f5423f05b1c8f0c18c52fffb33b7a8b2
karimouseyniibrahim <41455724+karimouseyniibrahim@users.noreply.github.com>
2019-05-06 11:52:19 +0200

    update 2

--- src/cup/example/expression/TSequencielle.java
+++ src/cup/example/expression/TSequencielle.java
@@ -6,36 +6,54 @@ public class TSequencielle extends AST {
 		
 		this.T=TYPE.sequence;
 		label.put(">>",">>P"+getIndex());
-		children.addAll(e1.children);
-		children.addAll(e2.children);
 		formula=label.toString();
 		ID =e1.formula+">>"+e2.formula;
 		ELEMENTS.put(ID, this);
 		graph.addNode(ID).setAttribute("ui.label",label.values() );
-		labels.putAll(e1.labels);
-		labels.putAll(e2.labels);
-		infoschildren.putAll(e1.infoschildren);
-		infoschildren.putAll(e2.infoschildren);
-		for (AST ast : e1.children) {	
-			if(ast.ID=="STOP"&&graph.getNode("STOP")==null) {
-				new TAtom("STOP",TYPE.stop);
+
+
+		//infoschildren.putAll(e1.children.size()==1?e1.);
+		//infoschildren.putAll(e2.infoschildren);
+		if(e1.children.size()==1){
+			labels.putAll(e1.labels);
+			children.addAll(e1.children);
+			labelsEdgets.putAll(e1.labelsEdgets);
+			for (AST ast : e1.children) {
+				if(ast.ID=="STOP"&&graph.getNode("STOP")==null) {
+					new TAtom("STOP",TYPE.stop);
+				}
+				graph.addEdge(ID+ast.ID, ID, ast.ID, true).setAttribute("ui.label",e1.labelsEdgets.get(ast.ID) );
 			}
-			graph.addEdge(ID+ast.ID, ID, ast.ID, true).setAttribute("ui.label",e1.labelsEdgets.get(ast.ID) );
+			if (graph.getNode(e1.ID) != null && (!(e1.ID.equals(StaticElements.STOP)))) {
+				graph.removeNode(e1.ID);
+			}
+		}else{
+			labels.put(e1.ID,e1.label);
+			children.add(e1);
+			labelsEdgets.put(e1.ID,"");
+			graph.addEdge(ID+e1.ID, ID, e1.ID, true).setAttribute("ui.label","" );
 		}
-		for (AST ast : e2.children) {
-			if(ast.ID=="STOP"&&graph.getNode("STOP")==null) {
-				new TAtom("STOP",TYPE.stop);
+		if(e2.children.size()==1) {
+			children.addAll(e2.children);
+			labels.putAll(e2.labels);
+			labelsEdgets.putAll(e1.labelsEdgets);
+			for (AST ast : e2.children) {
+				if (ast.ID == "STOP" && graph.getNode("STOP") == null) {
+					new TAtom("STOP", TYPE.stop);
+				}
+				if (graph.getEdge(ID + ast.ID) == null)
+					graph.addEdge(ID + ast.ID, ID, ast.ID, true).setAttribute("ui.label", e2.labelsEdgets.get(ast.ID));
 			}
-			if(graph.getEdge(ID+ast.ID)==null)
-				graph.addEdge(ID+ast.ID, ID, ast.ID, true).setAttribute("ui.label",e2.labelsEdgets.get(ast.ID) );
+
+			if (graph.getNode(e2.ID) != null && (!e2.ID.equals(StaticElements.STOP))) {
+				graph.removeNode(e2.ID);
+			}
+		}else{
+			labels.put(e2.ID,e2.label);
+			children.add(e2);
+			labelsEdgets.put(e2.ID,"");
+			graph.addEdge(ID+e2.ID, ID, e2.ID, true).setAttribute("ui.label","" );
 		}
-		if(graph.getNode(e1.ID)!=null&&(!(e1.ID.equals(StaticElements.STOP)))) {			
-			graph.removeNode(e1.ID);
-			}
-		if(graph.getNode(e2.ID)!=null&&(!e2.ID.equals(StaticElements.STOP))) {
-			graph.removeNode(e2.ID);
-			}
-		
 	}
 
 }
